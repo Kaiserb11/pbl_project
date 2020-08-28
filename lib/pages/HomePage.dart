ce0fd37c869a783031be15eb86943fe4efa3c792
Krishanu Bhattachajee <47850005+Kaiserb11@users.noreply.github.com>
2020-08-28 22:04:11 +0530

    added containers in home page

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -15,24 +15,149 @@ class _HomePageState extends State<HomePage> {
       
       body: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+          padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
           child: Container(
           width: MediaQuery.of(context).size.width,
           child: Column(
             children: [
               Container(
                 width: MediaQuery.of(context).size.width,
-                height: 150,
-                padding: EdgeInsets.all(10),
+                height: 200,
                 decoration: BoxDecoration(
                   color: Colors.purple[900],
                   borderRadius: BorderRadius.circular(10),
                 ),
-              )
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                padding: EdgeInsets.fromLTRB(0, 10 , 0, 0),
+                child: Row(
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width/2.6,
+                      height: 200,
+                      decoration: BoxDecoration(
+                        color: Colors.purple[800],
+                        borderRadius: BorderRadius.circular(10),
+                        
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(right: 10),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width/2.6,
+                      height: 200,
+                      decoration: BoxDecoration(
+                        color: Colors.purple[800],
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(right: 10),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width/2.6,
+                      height: 200,
+                      decoration: BoxDecoration(
+                        color: Colors.purple[800],
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                child: Padding(
+                  padding: EdgeInsets.only(top: 16, left: 5),
+                  child: Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                  'Find doctors in top speciality : ',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    ),
+                    ),
+                  ),
+                ),
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                padding: EdgeInsets.fromLTRB(0, 15 , 0, 20),
+                child: Row(
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width/2.6,
+                      height: 160,
+                      decoration: BoxDecoration(
+                        color: Colors.purple[800],
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(right: 10),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width/2.6,
+                      height: 160,
+                      decoration: BoxDecoration(
+                        color: Colors.purple[800],
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(right: 10),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width/2.6,
+                      height: 160,
+                      decoration: BoxDecoration(
+                        color: Colors.purple[800],
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(right: 10),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width/2.6,
+                      height: 160,
+                      decoration: BoxDecoration(
+                        color: Colors.purple[800],
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(right: 10),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width/2.6,
+                      height: 160,
+                      decoration: BoxDecoration(
+                        color: Colors.purple[800],
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(right: 10),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width/2.6,
+                      height: 160,
+                      decoration: BoxDecoration(
+                        color: Colors.purple[800],
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
-        )
+        ),
       ),
     );
   }
