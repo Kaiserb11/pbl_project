05533a6796214171ddc4b54ba1a945f7317efced
Krishanu Bhattachajee <47850005+Kaiserb11@users.noreply.github.com>
2020-09-01 19:34:47 +0530

    updated drawer menu

--- lib/widgets/drawerMenu.dart
+++ lib/widgets/drawerMenu.dart
@@ -5,32 +5,66 @@ class DrawerMenu extends StatelessWidget {
   Widget build(BuildContext context) {
     return Drawer(
       child: ListView(
+        padding: const EdgeInsets.all(0),
         children: [
-          DrawerHeader(
-            decoration: BoxDecoration(
-              color: Colors.white
-            ),
-            child: Text('hello'),
-          ),
-          ListTile(
-            title: Text(
-              'one'
-            ),
-          ),
-          ListTile(
-            title: Text(
-              'one'
+          UserAccountsDrawerHeader(
+            accountName: Text('Krishanu Bhatt'),
+            accountEmail: Text('dummyEmail@gmail.com'),
+            decoration: BoxDecoration(color: Colors.purple[800]),
+            currentAccountPicture: CircleAvatar(
+              backgroundColor: Colors.grey,
+              child: Icon(
+                Icons.person,
+                color: Colors.white,
+                size: 35,
+              ),
             ),
           ),
-          ListTile(
-            title: Text(
-              'one'
+          InkWell(
+            onTap: () {},
+            child: ListTile(
+              title: Text('Home'),
+              leading: Icon(Icons.home),
             ),
           ),
-          
+          InkWell(
+            onTap: () {},
+            child: ListTile(
+              title: Text('My Account'),
+              leading: Icon(Icons.person),
+            ),
+          ),
+          InkWell(
+            onTap: () {},
+            child: ListTile(
+              title: Text('Favorite'),
+              leading: Icon(Icons.favorite),
+            ),
+          ),
+          InkWell(
+            onTap: () {},
+            child: ListTile(
+              title: Text('My orders'),
+              leading: Icon(Icons.shopping_basket),
+            ),
+          ),
+          Divider(),
+          InkWell(
+            onTap: () {},
+            child: ListTile(
+              title: Text('Settings'),
+              leading: Icon(Icons.settings, color: Colors.green,),
+            ),
+          ),
+          InkWell(
+            onTap: () {},
+            child: ListTile(
+              title: Text('Exit'),
+              leading: Icon(Icons.exit_to_app, color: Colors.red,),
+            ),
+          ),
         ],
       ),
-      
     );
   }
 }
